d76aca54b969a802bc54289499ff491ecb4073fa
SampadaPimparwar <119919461+SampadaPimparwar@users.noreply.github.com>
2022-12-06 14:25:35 +0530

    sam

---
+++ AsMessApplication.java
@@ -0,0 +1,13 @@
+package com.Technosignia.AS.Mess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsMessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsMessApplication.class, args);
+	}
+
+}
